63d4fccf21c536f616d48f33e088b1445a7e61ad
Ubuntu <vagrant@Base-Box>
2018-12-06 10:17:54 +0000

    Added routes for the carts, cart_item and product

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,25 @@ Rails.application.routes.draw do
   # get 'products/new'
   # get 'products/edit'
 
+get 'carts/:id' => "carts#show", as: "cart"
+delete 'carts/:id' => "carts#destroy"
+
+post 'cart_items/:id/add' => "cart_items#add_quantity", as: "cart_item_add"
+post 'cart_items/:id/reduce' => "cart_items#reduce_quantity", as: "cartt_item_reduce"
+post 'cart_items' => "cart_items#create"
+get 'cart_items/:id' => "cart_items#show", as: "cart_item"
+delete 'cart_items/:id' => "cart_items#destroy"
+
+# Link from the Product index page (erb tags)
+button_to "Add to cart", cart_items_path(:product_id => product.id)
+
+# Link from the Cart show (erb tags)
+link_to "Add (+1)", cart_item_add_path(:id => cart_item), method: :post
+link_to "Reduce (-1)", cart_item_reduce_path(:id => cart_item), method: :post
+link_to "Remove item", cart_item_path(cart_item), method: :delete
+
+resources :products
+resources :orders
   devise_for :customers , :controllers => {registrations: "registrations"}
 
   resources :products ,only:[:index,:new]
