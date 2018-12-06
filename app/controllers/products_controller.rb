08609efb3a2711bdefa7cd4fa827681dc06b3b63
Ubuntu <vagrant@Base-Box>
2018-12-06 10:31:57 +0000

    Added CRUD informatioon in Product controller.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,30 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
-  def edit
+  def create
+    @product = Product.create(product_params)
+    redirect_to products_path
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to products_path
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :price, :image)
+    end
+    
 end
